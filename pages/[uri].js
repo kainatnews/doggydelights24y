31aabc9fec4c7f1fcba76f991702c67b76e9ec30
kainatnews <121275614+kainatnews@users.noreply.github.com>
2024-02-28 18:02:15 +0500

    Update [uri].js

--- pages/[uri].js
+++ pages/[uri].js
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 
 export default function SlugPage({ post }) {
 useEffect(()=>{
-  location = `https://nationkhabar.in/${post?.slug}`
+  location = `https://doggydelights.site/${post?.slug}`
 },[])
   return (
     <div>
